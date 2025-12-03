d5c7cdb5cf2fd3b7532688105a90032ec28bef39
Jainil Patil <jainilpatil510@gmail.com>
2025-12-03 19:35:10 +0530

    add POST /products endpoint using @RequestBody

--- src/main/java/com/example/productmanagement/controller/ProductController.java
+++ src/main/java/com/example/productmanagement/controller/ProductController.java
@@ -1,10 +1,7 @@
 package com.example.productmanagement.controller;
 
 import com.example.productmanagement.model.Product;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,33 +10,38 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
+
+    private List<Product> products = new ArrayList<>();
+
+    public ProductController() {
+        products.add(new Product(1, "MacBook Air", 999.99));
+        products.add(new Product(2, "iPhone 15", 799.99));
+        products.add(new Product(3, "AirPods Pro", 249.99));
+    }
+
     @GetMapping("/hello")
-    public String hello(){
+    public String hello() {
         return "Product API is running";
+    }
+
+    @PostMapping
+    public Product createProduct(@RequestBody Product product) {
+        products.add(product);
+        return product;
     }
 
     @GetMapping("/all")
     public List<Product> getAllProducts() {
-        List<Product> products = new ArrayList<>();
-        products.add(new Product(1, "MacBook Air", 999.99));
-        products.add(new Product(2, "iPhone 15", 799.99));
-        products.add(new Product(3, "AirPods Pro", 249.99));
-
         return products;
     }
-        @GetMapping("/{id}")
-        public Product getProductById(@PathVariable int id){
-            List<Product> products = new ArrayList<>();
-            products.add(new Product(1, "MacBook Air", 999.99));
-            products.add(new Product(2, "iPhone 15", 799.99));
-            products.add(new Product(3, "AirPods Pro", 249.99));
 
-            for (Product p :products) {
-                if(p.getId()==id){
-                    return p;
-                }
+    @GetMapping("/{id}")
+    public Product getProductById(@PathVariable int id) {
+        for (Product p : products) {
+            if (p.getId() == id) {
+                return p;
             }
-            return null;
         }
-
+        return null;
+    }
 }
