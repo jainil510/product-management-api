e4bf48ecb83f28957f30b5f057379db9d1677916
Jainil Patil <jainilpatil510@gmail.com>
2025-12-03 18:44:40 +0530

    add GET /products/{id} endpoint using @PathVariable

--- src/main/java/com/example/productmanagement/controller/ProductController.java
+++ src/main/java/com/example/productmanagement/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.example.productmanagement.controller;
 
 import com.example.productmanagement.model.Product;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,19 @@ public class ProductController {
 
         return products;
     }
+        @GetMapping("/{id}")
+        public Product getProductById(@PathVariable int id){
+            List<Product> products = new ArrayList<>();
+            products.add(new Product(1, "MacBook Air", 999.99));
+            products.add(new Product(2, "iPhone 15", 799.99));
+            products.add(new Product(3, "AirPods Pro", 249.99));
 
+            for (Product p :products) {
+                if(p.getId()==id){
+                    return p;
+                }
+            }
+            return null;
+        }
 
 }
